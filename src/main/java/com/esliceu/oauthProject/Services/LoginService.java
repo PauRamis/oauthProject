d2161e496c67532f64e21c8b80853f45af42abbf
pau <pauramisnunez@gmail.com>
2024-02-01 16:48:57 +0100

    uri start?

--- src/main/java/com/esliceu/oauthProject/Services/LoginService.java
+++ src/main/java/com/esliceu/oauthProject/Services/LoginService.java
@@ -1,10 +1,23 @@
 package com.esliceu.oauthProject.Services;
 
+import org.apache.http.client.utils.URIBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriBuilder;
 
 @Service
 public class LoginService {
-    public String getGoogleRedirection(){
+    @Value("${client-id}")
+    String clientId;
 
+    @Value("${redirect-uri}")
+    String redirecturi;
+    public String getGoogleRedirection() throws Exception{
+        //https://accounts.google.com/o/oauth2/v2/auth
+        URIBuilder b = new URIBuilder("https://accounts.google.com/o/oauth2/v2/auth");
+        b.addParameter("client_id", clientId);
+        b.addParameter("redirect_uri", redirecturi);
+
+        return "";
     }
 }
