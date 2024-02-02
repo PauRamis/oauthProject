d7d4111711f2ce92e9ea569298827b3e4887ecfe
pau <pauramisnunez@gmail.com>
2024-02-02 15:49:48 +0100

    5

--- src/main/java/com/esliceu/oauthProject/Services/LoginService.java
+++ src/main/java/com/esliceu/oauthProject/Services/LoginService.java
@@ -12,6 +12,9 @@ public class LoginService {
     @Value("${redirect-uri}")
     String redirecturi;
 
+    @Value("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")
+    String clientSecret;
+
     //Build get url
     public String getGoogleRedirection() throws Exception{
         URIBuilder b = new URIBuilder("https://accounts.google.com/o/oauth2/v2/auth");
@@ -23,6 +26,17 @@ public class LoginService {
         b.addParameter("response_type", "code");
         b.addParameter("prompt", "select_account");
         return b.build().toURL().toString();
+
+    }
+
+    public String getGoogleUserEmail(String code) throws Exception{
+        URIBuilder b = new URIBuilder("https://oauth2.googleapis.com/token");
+        b.addParameter("client_id", clientId);
+        b.addParameter("redirect_uri", redirecturi);
+        b.addParameter("code", code);
+        b.addParameter("client_secret", clientSecret);
+        b.addParameter("grant_type", "authorization_code");
+
 
     }
 }
