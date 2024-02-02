862d2d8a48ab36ec835578f63127bc82f152576f
pau <pauramisnunez@gmail.com>
2024-02-02 15:38:24 +0100

    success

--- src/main/java/com/esliceu/oauthProject/Controller/LoginController.java
+++ src/main/java/com/esliceu/oauthProject/Controller/LoginController.java
@@ -1,9 +1,11 @@
 package com.esliceu.oauthProject.Controller;
 
 import com.esliceu.oauthProject.Services.LoginService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
@@ -18,5 +20,18 @@ public class LoginController {
     public String logingoogle() throws Exception {
 
         return "redirect:" + loginService.getGoogleRedirection();
+    }
+
+    @GetMapping("/oauth2/callback")
+    public String googleCombecack(@RequestParam String code, HttpSession session){
+        String email = loginService.getGoogleUserEmail(code);
+        session.setAttribute("email", email);
+        return "redirect:/success";
+    }
+
+    @GetMapping("/success")
+    public String successController(HttpSession session){
+        if (session.getAttribute("email") == null) return "error";
+        return "success";
     }
 }
