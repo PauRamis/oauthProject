b33464aa0457920adbcaeb5f52d595abfab5d27a
pau <pauramisnunez@gmail.com>
2024-02-05 19:03:40 +0100

    try2 getDiscordUserEmail

--- src/main/java/com/esliceu/oauthProject/Services/LoginDiscordService.java
+++ src/main/java/com/esliceu/oauthProject/Services/LoginDiscordService.java
@@ -1,8 +1,25 @@
 package com.esliceu.oauthProject.Services;
 
+import com.google.gson.Gson;
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class LoginDiscordService {
@@ -31,37 +48,57 @@ public class LoginDiscordService {
     }
 
     public String getDiscordUserEmail(String code) throws Exception {
-        System.out.println("getDiscordUserEmail");
-        System.out.println(code);
-        String accessToken = exchangeCodeForToken(code);
-        return getDiscordUserInfo(accessToken);
+        URL url = new URL("https://discord.com/api/oauth2/token");
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put("client_id", clientId);
+        parameters.put("redirect_uri", redirecturi);
+        parameters.put("code", code);
+        parameters.put("client_secret", clientSecret);
+        parameters.put("grant_type", "authorization_code");
+
+        String result = doPost(url, parameters);
+
+        Map<String, String> map = new Gson()
+                .fromJson(result, HashMap.class);
+        String accessToken = map.get("access_token");
+
+        URIBuilder userInfoBuilder = new URIBuilder("https://discord.com/api/users/@me");
+        String userInfoResponse = doGet(userInfoBuilder.setParameters(List.of(new BasicNameValuePair("Authorization", "Bearer " + accessToken))).build().toURL());
+
+        Map<String, String> userInfoMap = new Gson()
+                .fromJson(userInfoResponse, HashMap.class);
+
+        return userInfoMap.get("email");
     }
 
-    private String exchangeCodeForToken(String code) throws Exception {
-        URIBuilder uriBuilder = new URIBuilder("https://discord.com/api/oauth2/token");
-        uriBuilder.addParameter("client_id", clientId);
-        uriBuilder.addParameter("client_secret", clientSecret);
-        uriBuilder.addParameter("grant_type", "authorization_code");
-        uriBuilder.addParameter("code", code);
-        uriBuilder.addParameter("redirect_uri", redirecturi + "/discord/callback");
+    private String doGet(URL url) throws Exception {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet get = new HttpGet(url.toString());
+        CloseableHttpResponse response = httpClient.execute(get);
 
-        // Realiza la solicitud HTTP POST para intercambiar el código por un token de acceso
-        // (Implementa esta lógica según la biblioteca o método que estés utilizando)
-
-        // Devuelve el token de acceso obtenido
-        // (Implementa esta lógica según la biblioteca o método que estés utilizando)
-        return null;
+        if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+            return EntityUtils.toString(response.getEntity());
+        }
+        throw new RuntimeException("Error in response");
     }
 
-    private String getDiscordUserInfo(String accessToken) throws Exception {
-        URIBuilder uriBuilder = new URIBuilder("https://discord.com/api/users/@me");
+    private String doPost(URL url, Map<String, String> parameters) throws Exception {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpPost post = new HttpPost(url.toString());
 
-        // Realiza la solicitud HTTP GET a la API de Discord para obtener información del usuario
-        // (Implementa esta lógica según la biblioteca o método que estés utilizando)
+        List<NameValuePair> nvps = new ArrayList<>();
+        for (Map.Entry<String, String> entry : parameters.entrySet()) {
+            nvps.add(new BasicNameValuePair(entry.getKey(), entry.getValue()));
+        }
 
-        // Devuelve el correo electrónico del usuario obtenido de la respuesta
-        // (Implementa esta lógica según la biblioteca o método que estés utilizando)
-        return null;
+        post.setEntity(new UrlEncodedFormEntity(nvps));
+
+        CloseableHttpResponse response = httpClient.execute(post);
+
+        if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+            return EntityUtils.toString(response.getEntity());
+        }
+        throw new RuntimeException("Error in response");
     }
 
 }
